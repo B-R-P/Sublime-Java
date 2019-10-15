93b28b540831c35cf115db3a1caf3443793ffece
Bijin123 <bijinregipanicker@gmail.com>
2019-10-16 00:04:14 +0530

    Delete CallAll.bat

--- CallAll.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mkdir build 2> NUL
-cd build && call javac -d . -Xlint ..\*.java && echo Main-Class: %1 >..\manifest.txt && jar cvfm ..\%1.jar ..\manifest.txt * >NUL && echo Output: && java -jar ..\%1.jar
-del ..\manifest.txt
